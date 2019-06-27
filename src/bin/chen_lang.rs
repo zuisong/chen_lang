38725d58ea0f7fd80dcb4669a49f9259700baf19
zuisong <378818573@qq.com>
2019-06-27 10:23:33 +0800

    命令行部分用 clap 可以提供更人性化的提示

--- src/bin/chen_lang.rs
+++ src/bin/chen_lang.rs
@@ -2,19 +2,47 @@ use log::Level;
 use log::*;
 use std::fs::OpenOptions;
 use std::io::Read;
+
+extern crate clap;
+
+use clap::{App, Arg};
+
 fn main() -> Result<(), failure::Error> {
-    simple_logger::init_with_level(Level::Warn)?;
-    let args: Vec<_> = std::env::args().collect();
-    if args.len() < 2 {
-        panic!("请指定要执行的源代码文件");
-    }
-    let s = std::env::current_dir()?.join(&args[1]);
+    let matches = App::new("chen_lang")
+        .version("0.0.1")
+        .author("zuisong <com.me@foxmail.com>")
+        .about("a super tiny and toy language write by rust")
+        .arg(
+            Arg::with_name("INPUT_FILE")
+                .help("要执行的源代码文件")
+                .required(true)
+                .index(1),
+        )
+        .arg(
+            Arg::with_name("v")
+                .short("v")
+                .required(false)
+                .multiple(true)
+                .help("v越多日志级别越低"),
+        )
+        .get_matches();
+    let log_level = match matches.occurrences_of("v") {
+        0 => Level::Error,
+        1 => Level::Warn,
+        2 => Level::Info,
+        3 => Level::Debug,
+        4 | _ => Level::Trace,
+    };
+
+    simple_logger::init_with_level(log_level)?;
+
+    let code_file = matches.value_of("INPUT_FILE").unwrap();
+    let s = std::env::current_dir()?.join(code_file);
 
     debug!("{:?}", s);
     let mut f = OpenOptions::new().read(true).open(s)?;
 
     let mut v = vec![];
-
     f.read_to_end(&mut v)?;
     let code = String::from_utf8(v)?;
 
