401d74bb66dc207fa7d452ebb5ae5d81199728fd
zuisong <com.me@foxmail.com>
2023-02-17 23:08:46 +0800

    clap_complete

--- src/bin/chen_lang.rs
+++ src/bin/chen_lang.rs
@@ -1,31 +1,41 @@
 extern crate clap;
 
 use std::fs::OpenOptions;
+use std::io;
 use std::io::Read;
+use clap_complete::{generate, Generator, Shell};
 
-use clap::{Arg, Command};
+use clap::{Command, Arg, value_parser, ArgAction};
 use log::*;
 use simple_logger::SimpleLogger;
 
 fn main() -> Result<(), anyhow::Error> {
-    let matches = Command::new("chen_lang")
+    let mut cmd = Command::new("chen_lang")
         .version("0.0.1")
         .author("zuisong <com.me@foxmail.com>")
         .about("a super tiny and toy language write by rust")
         .arg(
-            Arg::new("INPUT_FILE")
+            Arg::new("run")
+                .action(ArgAction::Set)
+                .long("run")
                 .help("要执行的源代码文件")
-                .required(true)
-                .index(1),
+                .required(false)
         )
         .arg(
             Arg::new("v")
                 .short('v')
-                .action(clap::ArgAction::Count)
+                .action(ArgAction::Count)
                 .required(false)
                 .help("v越多日志级别越低 (-vv is Info, -vvv is Debug)"),
         )
-        .get_matches();
+        .arg(
+            Arg::new("generator")
+                .required(false)
+                .long("generate")
+                .action(ArgAction::Set)
+                .value_parser(value_parser!(Shell)),
+        );
+    let matches = cmd.clone().get_matches();
 
     let log_level = match matches.get_count("v") {
         0 => LevelFilter::Error,
@@ -35,20 +45,31 @@ fn main() -> Result<(), anyhow::Error> {
         4 => LevelFilter::Trace,
         _ => LevelFilter::Trace,
     };
-
     SimpleLogger::new().with_level(log_level).init().unwrap();
 
-    let code_file = matches.get_one::<String>("INPUT_FILE").unwrap();
-    let s = std::env::current_dir()?.join(code_file);
+    if let Some(generator) = matches.get_one::<Shell>("generator").copied() {
+        eprintln!("Generating completion file for {}...", generator);
+        print_completions(generator, &mut cmd);
+    }
 
-    debug!("{:?}", s);
-    let mut f = OpenOptions::new().read(true).open(s)?;
 
-    let mut v = vec![];
-    f.read_to_end(&mut v)?;
-    let code = String::from_utf8(v)?;
+    if let Some(code_file) = matches.get_one::<String>("run") {
+        let s = std::env::current_dir()?.join(code_file);
 
-    debug!("{:?}", code);
-    chen_lang::run(code)?;
+        debug!("{:?}", s);
+        let mut f = OpenOptions::new().read(true).open(s)?;
+
+        let mut v = vec![];
+        f.read_to_end(&mut v)?;
+        let code = String::from_utf8(v)?;
+
+        debug!("{:?}", code);
+        chen_lang::run(code)?;
+    }
     Ok(())
+}
+
+
+fn print_completions<G: Generator>(gen: G, cmd: &mut Command) {
+    generate(gen, cmd, cmd.get_name().to_string(), &mut io::stdout());
 }
---
+++ src/wasm/main.rs
@@ -0,0 +1,23 @@
+extern crate chen_lang;
+
+use std::ffi::{CStr, CString};
+use std::mem;
+use std::{rc::Rc, os::raw::{c_char, c_void}};
+
+fn main() {}
+
+fn string_to_ptr(s: String) -> *mut c_char {
+    CString::new(s).unwrap().into_raw()
+}
+
+#[no_mangle]
+pub fn eval(input_ptr: *mut c_char) -> *mut c_char {
+    let input = unsafe { CStr::from_ptr(input_ptr).to_string_lossy().into_owned() };
+
+    match chen_lang::run(input) {
+        Ok(_) => {}
+        Err(_) => {}
+    }
+
+    string_to_ptr("OK".to_string())
+}
